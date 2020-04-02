ec392429bf7c4dc330a30e85259de88edb05ca6d
lihengyu <lhygoogle@163.com>
2020-04-02 19:29:15 +0800

    二哥看一眼

--- src/views/General.vue
+++ src/views/General.vue
@@ -1,47 +1,55 @@
 <template>
-  <div class="General">
-    <div class="Gcenter">
-      数据更新时间：<b>2020.01.01  00:00:00</b>
+  <div>
+    <div class="title-line mg-b10">
+      数据更新时间：<span>{{ form.updateTime }}</span>
     </div>
-    <div class="Gbottom">
-      <div class="Gls">
-        <div
-          v-for="(item,index) in lists"
-          :key="index">
-          <div class="Gtit">
-            {{ item.title }}
-          </div>
-          <div class="num">
-            {{ item.num }}
-          </div>
-          <div class="num1">
-            <span>{{ item.content.text ? `${item.content.text}:` : '' }}</span><span class="green">{{ item.content.num1 }}</span>
-          </div>
-        </div>
-      </div>
-      <div class="Gcontent">
-        <div class="Gleft">
-          <Practices></Practices>
-          <Histograms></Histograms>
-        </div>
-        <div class="Gcente">
-          <Map></Map>
-        </div>
-        <div class="Gright">
-          <PieCharts></PieCharts>
-          <Transverse></Transverse>
-        </div>
-      </div>
-      <div class="Gnext">
+
+    <el-row
+      :gutter="gutter"
+      class="mg-b10">
+      <el-col
+        v-for="(item, index) in lists"
+        :key="index"
+        :span="6">
+        <title-box
+          :item="item"
+          :data="form"></title-box>
+      </el-col>
+    </el-row>
+
+    <el-row
+      :gutter="gutter"
+      class="mg-b10">
+      <el-col :span="6">
+        <Practices></Practices>
+        <Histograms class="mg-t10"></Histograms>
+      </el-col>
+      <el-col
+        :span="12"
+        class="map-container">
+        <Map></Map>
+      </el-col>
+      <el-col :span="6">
+        <PieCharts></PieCharts>
+        <Transverse class="mg-t10"></Transverse>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="gutter">
+      <el-col :span="12">
         <Columnars></Columnars>
+      </el-col>
+      <el-col :span="12">
         <Lines></Lines>
-      </div>
-    </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-  import Practices from './components/Practices' // 环状饼图
+  import TitleBox from './components/TitleBox'
+
+  import Practices from './components/Ipractices' // 环状饼图
   import PieCharts from './components/PieCharts' // 饼图
   import Histograms from './components/Histograms' // 横向柱状图
   import Columnars from './components/Columnars' // 底部左侧柱状图
@@ -52,6 +60,7 @@
   export default {
     name: 'General',
     components: {
+      TitleBox,
       Practices,
       PieCharts,
       Histograms,
@@ -62,29 +71,35 @@
     },
     data () {
       return {
+        gutter: 10,
+        form: {
+          updateTime: '2020.01.01  00:00:00',
+          allUsers: '100000',
+          dayCount: '75920',
+          allFlow: '8000',
+          allFunNum: '80000',
+          peopleOnLineTime: '1小时54分钟'
+        },
         lists: [
           {
             title: '车机系统总用户数',
-            num: '100000',
-            content: {
-              text: '日活',
-              num1: '75920'
-            }
+            prop: 'allUsers',
+            content: [{
+              label: '日活',
+              prop: 'dayCount'
+            }]
           },
           {
             title: '总流量消耗',
-            num: '8000',
-            content: {}
+            prop: 'allFlow'
           },
           {
             title: '总粉丝数',
-            num: '80000',
-            content: {}
+            prop: 'allFunNum'
           },
           {
             title: '2020年日人均在线时长',
-            num: '1小时54分钟',
-            content: {}
+            prop: 'peopleOnLineTime'
           }
         ]
       }
@@ -92,81 +107,23 @@
   }
 </script>
 
-<style scoped>
-  .General{
-    box-sizing: border-box;
-  }
-  .Gtop{
+<style scoped lang="scss">
+  .title-line{
     text-align: left;
-    height: 35px;
-    line-height: 32px;
-  }
-  .Gtop>span{
-    display: inline-block;
-    width: 90px;
-    height: 32px;
-    text-align: center;
-    border:1px solid #ccc;
-    border-radius:5px;
-    background-color: #fff;
-  }
-  .Gcenter{
-    text-align: left;
-    line-height: 50px;
+    line-height: 30px;
     background-color: #fff;
     padding: 0 15px;
+    span {
+      font-weight: bold;
+    }
   }
-  .Gls{
-    height: 120px;
-    display: flex;
-    justify-content: space-between;
-    text-align: center;
-    margin-top:15px;
-    margin-bottom:15px;
+  .map-container {
+    height: 490px;
   }
-  .Gls>div{
-    width: 24%;
-    height: 100%;
-    background-color: #fff;
+  .mg-b10 {
+    margin-bottom: 10px;
   }
-  .Gtit{
-    font-weight: bold;
-    font-size: 18px;
-    line-height: 40px;
-  }
-  .num{
-    font-weight: bold;
-    color:rgb(27, 173, 231);
-    font-size: 30px;
-  }
-  .Gcontent{
-    display: flex;
-    justify-content: space-between;
-  }
-  .Gleft{
-    width: 24%;
-  }
-  .Gright{
-    width: 24%;
-  }
-  .Gnext{
-    height: 240px;
-    display: flex;
-    justify-content: space-between;
-  }
-  .Gcontent>.Gcente{
-    width: 49.5%;
-    height: 495px;
-    background-color: #fff;
-  }
-  .num1{
-    font-size: 12px;
-    text-align-last: left;
-    margin-left: 85px;
-    line-height: 30px;
-  }
-  .green{
-    color:rgb(23, 194, 23);
-    font-weight: bold;
+  .mg-t10 {
+    margin-top: 10px;
   }
 </style>
---
+++ src/views/components/TitleBox.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="title-box">
+    <div class="title">
+      {{ item.title }}
+    </div>
+    <div class="value">
+      {{ showText(item) }}
+    </div>
+    <el-row v-if="showContent">
+      <el-col
+        v-for="(info, index) in item.content"
+        :key="index"
+        :span="8"
+        class="info">
+        <span>{{ info.label }}:</span>
+        <span class="info-value">{{ showText(info) }}</span>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TitleBox',
+    props: {
+      item: {
+        type: Object,
+        default: function () {
+          return {}
+        }
+      },
+      data: {
+        type: Object,
+        default: function () {
+          return {}
+        }
+      }
+    },
+    computed: {
+      showContent () {
+        return this.item.content && this.item.content.length > 0
+      }
+    },
+    methods: {
+      showText ({ prop }) {
+        if (!this.data[prop]) {
+          return '--'
+        }
+        return this.data[prop]
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .title-box {
+    background: white;
+    height: 120px;
+    padding: 10px;
+    text-align: center;
+    .title {
+      font-weight: bold;
+      font-size: 18px;
+      line-height: 40px;
+    }
+    .value {
+      font-weight: bold;
+      color: #409EFF;
+      font-size: 30px;
+    }
+    .info {
+      font-size: 12px;
+      line-height: 30px;
+      &-value {
+        color: #17C217;
+        font-weight: bold;
+      }
+    }
+  }
+</style>
